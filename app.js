904afd8c0859379354448a8c7d96369e5bf20635
Cassul <aysa.garden@gmail.com>
2018-02-26 13:13:49 +1100

    port variable set

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-const PORT = 8080;
+// const PORT = 8080;
 const path = require('path');
 const exchangesInfo = require('./test.js');
-
+app.set('port', process.env.PORT || 3000);
+const server = app.listen(app.get('port'), function() {
+  console.log('listening to port ' + app.get('port'));
+  });
 app.set('views', './views');
 app.set('view engine', 'ejs');
-app.listen(PORT, function () {
-  console.log(`app is listening on ${PORT}`);
-});
+// app.listen(PORT, function () {
+//   console.log(`app is listening on ${PORT}`);
+// });
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', (req,res) => res.send(exchangesInfo));
