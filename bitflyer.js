1c3c3f616a40af2069b9419167dfad78518c3245
Cassul <aysa.garden@gmail.com>
2018-03-06 13:06:36 +1100

    bug with bitflyer fixed

--- bitflyer.js
+++ bitflyer.js
@@ -3,7 +3,7 @@ module.exports = exchangesInfo.then( function (exchangesInfo) {
 var bitflyer;
 var one = ["binance", "okex", "hitbtc", "poloniex", "bitmex", "bitfinex", "gdax", "kraken", "bitstamp"];
 var x = exchangesInfo.bitflyer.best_ask;
-var allDif = [exchangesInfo.binance.bidPrice - x, exchangesInfo.okex.buy - x, exchangesInfo.hitbtc.bid - x, exchangesInfo.poloniex.highestBid - x, exchangesInfo.bitmex.close - x, exchangesInfo.bitfinex.bid - x, exchangesInfo.gdax.bid - x, exchangesInfo.kraken.b[0] - x, exchangesInfo.bitstamp.bid] - x;
+var allDif = [exchangesInfo.binance.bidPrice - x, exchangesInfo.okex.buy - x, exchangesInfo.hitbtc.bid - x, exchangesInfo.poloniex.highestBid - x, exchangesInfo.bitmex.close - x, exchangesInfo.bitfinex.bid - x, exchangesInfo.gdax.bid - x, exchangesInfo.kraken.b[0] - x, exchangesInfo.bitstamp.bid - x];
 
 
   bitflyer = [one, allDif];
