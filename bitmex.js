9a29d4d6f72089e8d036a2ad6796193bf7fadb7a
Cassul <aysa.garden@gmail.com>
2018-03-06 13:15:52 +1100

    bitmex bug fixed

--- bitmex.js
+++ bitmex.js
@@ -3,7 +3,7 @@ module.exports = exchangesInfo.then( function (exchangesInfo) {
 var bitmex;
 var one = ["binance", "okex", "hitbtc", "poloniex", "bitfinex", "gdax", "kraken", "bitflyer", "bitstamp"];
 var x = exchangesInfo.bitmex.high;
-var allDif = [exchangesInfo.binance.bidPrice - x, exchangesInfo.okex.buy - x, exchangesInfo.hitbtc.bid - x, exchangesInfo.poloniex.highestBid - x, exchangesInfo.bitfinex.bid - x, exchangesInfo.gdax.bid - x, exchangesInfo.kraken.b[0] - x, exchangesInfo.bitflyer.best_bid - x, exchangesInfo.bitstamp.bid] - x;
+var allDif = [exchangesInfo.binance.bidPrice - x, exchangesInfo.okex.buy - x, exchangesInfo.hitbtc.bid - x, exchangesInfo.poloniex.highestBid - x, exchangesInfo.bitfinex.bid - x, exchangesInfo.gdax.bid - x, exchangesInfo.kraken.b[0] - x, exchangesInfo.bitflyer.best_bid - x, exchangesInfo.bitstamp.bid - x];
 
 
   bitmex = [one, allDif];
